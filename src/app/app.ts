ee006794131c4d5e06bcc59e051bfec8f80d9e66
alecmarredo <alecmarredo@gmail.com>
2025-09-19 17:10:33 -0300

    trabajando en angular

---
+++ src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+//import { RouterOutlet } from '@angular/router';
+import { Aside } from './componentes/aside/aside';
+import { Main } from './componentes/main/main';
+import { Footer } from './componentes/footer/footer';
+import { Navegacion } from './componentes/navegacion/navegacion';
+
+@Component({
+  selector: 'app-root',
+  imports: [Navegacion,Aside,Main,Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('proyectoAlecMarredo');
+}
